50fc46dc254a2a6f8147de969a4f7d57caf8b6a5
chenxian <chenxian_fd@163.com>
2022-04-23 14:29:01 +0800

    first init

---
+++ src/main/java/com/eva/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.eva.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.eva.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
